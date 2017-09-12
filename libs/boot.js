eb7930b59a6f8c2a3736d0c77bf6e747fd4de66a
Test <Test>
2017-09-12 22:16:31 +0300

    port

--- libs/boot.js
+++ libs/boot.js
@@ -1,5 +1,5 @@
 module.exports = app => {
   app.db.sequelize.sync().done(() => {
-    app.listen(process.env.port || 3000, () => console.log('API app started'));
+    app.listen(process.env.port || 8080, () => console.log('API app started'));
   });
 };
